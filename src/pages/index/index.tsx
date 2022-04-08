733d281881e693d3496aa9f83a984cadc4dd57d8
zhanghao.gino <zhanghao.gino@bytedance.com>
2022-04-08 17:35:58 +0800

    修改为在 render 函数中触发
    Change-Id: If5bcfc120549939fdd112531e91aab0e52bf44f3

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,12 +4,14 @@ import { useState, useEffect } from "react";
 let time = 1;
 function Index() {
   const [time1] = useState(time);
+  const [render, setRender] = useState(false);
   useEffect(() => {
     time++;
   }, []);
-  setInterval(() => {
-    console.log('index',time1);
+  setTimeout(() => {
+    setRender(!render);
   }, 1000);
+  console.log('index',time1);
   return <View>首页</View>;
 }
 
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -5,12 +5,14 @@ import Taro from '@tarojs/taro';
 let time = 1;
 function Index() {
   const [time1] = useState(time);
+  const [render, setRender] = useState(false);
   useEffect(() => {
     time++;
   }, []);
-  setInterval(() => {
-    console.log('test',time1);
+  setTimeout(() => {
+    setRender(!render);
   }, 1000);
+  console.log('test',time1);
   return (
     <>
       <View onClick={() => {Taro.reLaunch({url: '/pages/index/index'})}}>重启到首页</View>
