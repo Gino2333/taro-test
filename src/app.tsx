49369a845361980995a7d660b3f7e649c021f8b0
zhanghao.gino <zhanghao.gino@bytedance.com>
2022-04-12 14:14:44 +0800

    增加测试
    Change-Id: Ibd1350041c66b0d96ce57ca05d9b8929228bbfbe

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,14 @@
 // 全局样式
+import { useEffect } from "react";
 import "./app.less";
 
 function App(props) {
+  useEffect(() => {
+    console.log("app launch");
+    return () => {
+      console.log("app unload");
+    };
+  }, []);
   return (
     // 在入口组件不会渲染任何内容，但我们可以在这里做类似于状态管理的事情
     <>
